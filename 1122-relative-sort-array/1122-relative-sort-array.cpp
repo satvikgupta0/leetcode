150551dbc35b55146e9ca48270b04f0f0df8e495
Satvik Gupta <satvikg152@gmail.com>
2026-07-10 11:55:57 +0530

    Time: 5 ms (1.33%), Space: 11.1 MB (65.64%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> a;
+        for(int x:arr2){
+            for(int i=0;i<arr1.size();i++){
+                if(arr1[i]==x){
+                    a.push_back(arr1[i]);
+                    arr1[i]=-1;
+                }
+            }
+        }
+        vector<int> rem;
+        for(int i=0;i<arr1.size();i++){
+            if(arr1[i]!=-1){
+                rem.push_back(arr1[i]);
+            }
+        }
+        sort(rem.begin(),rem.end());
+        for(int i=0;i<rem.size();i++){
+            a.push_back(rem[i]);
+        }
+        
+        return a;
+    }
+};
