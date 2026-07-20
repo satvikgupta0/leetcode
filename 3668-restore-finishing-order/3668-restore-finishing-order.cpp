4d9d91995a3692323b9741662152e0d2df5f6a11
Satvik Gupta <satvikg152@gmail.com>
2026-07-20 14:42:49 +0530

    Time: 2 ms (39.37%), Space: 50.4 MB (42.97%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        set<int> s(friends.begin(),friends.end());
+        vector<int> ans;
+        for(int x:order){
+            if(s.count(x))
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
